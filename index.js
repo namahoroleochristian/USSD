1191f68916eeeef8bc159ccb462c299bb2829e1d
namahoroleochristian <leonamahoro1@gmail.com>
2025-08-01 23:54:58 +0200

    route fixes

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { run } from "./src/config/db.js";
+import { ussdRoutes } from "./src/routes/ussd.routes.js";
 
 dotenv.config();
 const app = express();
 run()
-
+app.use(cors());
+app.use(express.json());
+app.use("/api", ussdRoutes);
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
 });
--- src/routes/ussd.routes.js
+++ src/routes/ussd.routes.js
@@ -1,2 +1,10 @@
 import express from "express";
-const ussdRoutes = express.Router();
+import { HandleUSSDCall } from "../controllers/ussd.controller.js";
+export const ussdRoutes = express.Router()
+ussdRoutes.post("/ussd", (req, res) => {
+    const { sessionId, serviceCode, phoneNumber, text } = req.body;
+    console.log(`Session ID: ${sessionId}, Service Code: ${serviceCode}, Phone Number: ${phoneNumber}, Text: ${text}`);
+    
+    // Call the controller function to handle the USSD request
+    HandleUSSDCall(req, res);
+});
